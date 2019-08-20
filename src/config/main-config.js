a045bb17ca6907017f03947062e8712873245562
Chakots <nritya.k@gmail.com>
2019-08-19 21:32:27 -0700

    Signup view renders

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 const logger = require('morgan');
+const path = require("path");
+const viewsFolder = path.join(__dirname, "..", "views");
+const bodyParser = require("body-parser");
 
 
 module.exports = {
     init(app){
-  
-     app.use(logger('dev'));
-       
+        app.set("views", viewsFolder);
+        app.set("view engine", "ejs");
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(logger('dev'));
      }
   };
   
